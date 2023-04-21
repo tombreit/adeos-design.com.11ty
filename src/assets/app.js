65429c28e54c9c842db69d4a77ae20897d7c9e8d
Thomas Breitner <mail@thms.de>
2023-04-21 23:38:54 +0200

    navbar sticky top, offcanvas with anchor navigation. needed to manually close offcanvas elem after nav-item clicked via js

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,3 +1,16 @@
 // TODO: Import only used components
 
 import * as bootstrap from 'bootstrap';
+
+
+const offcanvasElem = document.querySelector(".offcanvas-start");
+const offcanvasNavLinksContainer = document.querySelector(".offcanvas-body nav");
+const bsOffcanvas = new bootstrap.Offcanvas(offcanvasElem)
+
+offcanvasNavLinksContainer.addEventListener('click', function (event) {
+	// Ignore all elements without .nav-item class
+	if (!event.target.closest('.nav-item')) return;
+
+	// ...and close our bs offcanvas when clicked on a .nav-item link
+    bsOffcanvas.hide();
+});
